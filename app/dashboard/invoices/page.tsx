5391847b71426a1d7b58184e871cddd25b19d302
cjg <272372305@qq.com>
2024-07-26 15:03:49 +0800

    09.流式传输

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,8 +4,8 @@ export const metadata: Metadata = {
   title: "发票",
 };
 
-export default function Page() {
+export default async function Page() {
   // throw Error("This file is not used anymore. Please remove it.");
-
+  await new Promise((res) => setTimeout(res, 3000));
   return <p>Invoices Page</p>;
 }
