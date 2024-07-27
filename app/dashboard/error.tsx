0f0e6ac539d775f82ca9cedfd660ac112478955f
cjg <272372305@qq.com>
2024-07-27 14:23:08 +0800

    13.处理错误

--- app/dashboard/error.tsx
+++ app/dashboard/error.tsx
@@ -1,4 +1,6 @@
-"use client"; // Error components must be Client Components
+"use client";
+
+import { useEffect } from "react";
 
 export default function Error({
   error,
@@ -7,20 +9,23 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <p>
-        <span className="bg-red-500">{error.message}</span>
-      </p>
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Something went wrong!</h2>
       <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={
-          // Attempt to recover by trying to re-render the segment
+          // Attempt to recover by trying to re-render the invoices route
           () => reset()
         }
       >
         Try again
       </button>
-    </div>
+    </main>
   );
 }
